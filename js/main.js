200186ed96e59eab0eb6b4fd2d104a8847c8d240
Flavio Muscente <flaviomuscente@libero.it>
2020-12-01 18:00:11 +0100

    simplified code

--- js/main.js
+++ js/main.js
@@ -2,16 +2,12 @@ var app = new Vue({
   el: '#app',
   data: {
     musics:[],
-    cont: 0,
     genSel: "All"
   },
   mounted(){
     //prelevo i dati tramite axios e li inserisco nel mio array
     axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((mySong)=> {
-      while(this.cont < mySong.data.response.length){
-        this.musics.push(mySong.data.response[this.cont]);
-        this.cont++;
-      }
+        this.musics = mySong.data.response;
     });
 
   },
